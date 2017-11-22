00b52fc06acc98ed2fbb41da93502f5758437f96
Pravin Kumar Gnanaolivu (Product Engineering Service) <PR391830@wipro.com>
2017-11-22 10:47:22 +0530

    updated successfully

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,8 @@
 class Helloworld{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     int i = 0;
+     for(i=0; i< 10; i++){
+        System.out.println(i);
+      }
     }  
 }  
