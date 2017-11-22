f64cee5cd61a547973565bdcc37690628dc423b7
Pravin Kumar Gnanaolivu (Product Engineering Service) <PR391830@wipro.com>
2017-11-22 12:38:50 +0530

    File Changed

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,8 @@
 class Helloworld{  
     public static void main(String args[]){  
-     int i = 0;
+     int i = 0,sum=0;
      for(i=0; i< 10; i++){
-        System.out.println(i);
+        System.out.println(sum = sum + i);
       }
     }  
 }  
